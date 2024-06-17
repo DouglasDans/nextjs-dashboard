8ecf68bf66e9b54014fe91e64128ebe22c5e1fae
douglas.dans <douglas.dans@gmail.com>
2024-06-17 15:58:20 -0300

    Cap 15 e 16 feitos

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import { Metadata } from 'next';
+
+export const metadata : Metadata = ({
+  title: "Customers"
+})
+
 type Props = {}
 
 export default function Customers({}: Props) {
